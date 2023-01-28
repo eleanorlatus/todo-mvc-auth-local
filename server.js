d46dd6b58e51356818972b3ac7d4e6080c4c603b
Eleanor Latus <eleanor@latus.co.uk>
2023-01-28 11:30:57 +0000

    comments added

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
+//handles cookie/session to see if a user is logged in
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
+//error message for incorrect password etc
 const flash = require('express-flash')
+// logs the requests
 const logger = require('morgan')
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
@@ -21,6 +24,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+//runs morgan
 app.use(logger('dev'))
 // Sessions
 app.use(
@@ -28,6 +32,7 @@ app.use(
       secret: 'keyboard cat',
       resave: false,
       saveUninitialized: false,
+      //stores each session in mongoose
       store: new MongoStore({ mongooseConnection: mongoose.connection }),
     })
   )
@@ -35,12 +40,12 @@ app.use(
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
-
+//runs flash alerts
 app.use(flash())
-  
+// routes set up
 app.use('/', mainRoutes)
 app.use('/todos', todoRoutes)
- 
+
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it!')
 })    
